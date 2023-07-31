1165d5f1a8ee512878bfa803116f13e22655be24
Maarij-Camroodien <maarijcamroodien18@gmail.com>
2023-07-31 15:51:02 +0200

    commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const Person = require('./person')
+
+    const newPerson1 = new Person('Maarij', 'Camroodien', 'maarijcamroodien18@gmail.com')
+    const newPerson2 = new Person('John', 'Doe', 'johndoe@gmail.com')
+    const newPerson3 = new Person('Sam', 'Samsodien', 'samsamsodien@gmail.com')
+    console.log(newPerson1, newPerson2, newPerson3);
